fd7f424b269069f372545ad3324baf8553bb5782
Nathalie Raffray <nathalie.raffray9@gmail.com>
2019-07-19 19:03:52 -0400

    figured out the frickin multiple background color thing for the transaction screen items

--- Transaction/transaction.js
+++ Transaction/transaction.js
@@ -47,16 +47,33 @@ $('#transactionScreen').on('click', '#transactionTable tr:not(first-child)', fun
   // if(this.style.borderBottom = '1px solid rgb(125, 170, 108)'){ //then the thing
   //
   // }
-  this.style.borderBottom = '1px solid rgb(125, 170, 108)';
-  this.style.backgroundImage = 'linear-gradient(rgb(104, 168, 94), rgb(74, 122, 67))';
+  this.style.borderBottom = 'none, 1px solid rgb(125, 170, 108)';
+  $(this).css('background', 'linear-gradient(rgb(104, 168, 94), rgb(74, 122, 67)) bottom center / auto 51% no-repeat, linear-gradient(rgb(197, 251, 172), rgb(119, 184, 108)) top center / auto 49% no-repeat');
   $(this).find('td:nth-child(2)').css('color', 'black');
+  // $(this).css('background-size','auto 51%, auto 49%');
+  // $(this).css('background-repeat','no-repeat');
 
-  if(previousTransactionRowSelected != undefined && previousTransactionRowSelected.style != undefined){
+  // background-image:linear-gradient(rgb(104, 168, 94), rgb(74, 122, 67)), linear-gradient(rgb(197, 251, 172), rgb(119, 184, 108));
+	// background-position:bottom center, top center;
+  // background-size: auto 50%, auto 50%;
+  // border-bottom: none, 1px solid rgb(125, 170, 108);
+  // background-repeat: no-repeat;
+
+  if(previousTransactionRowSelected != undefined && previousTransactionRowSelected.style != undefined && previousTransactionRowSelected != this){
     previousTransactionRowSelected.style.borderBottom = '1px solid rgb(43, 43, 42)';
-    previousTransactionRowSelected.style.backgroundImage = 'linear-gradient(rgb(40, 40, 40), rgb(85, 85, 85))';
+    $(previousTransactionRowSelected).css('background', 'linear-gradient(rgb(40, 40, 40), rgb(85, 85, 85))');
     $(previousTransactionRowSelected).find('td:nth-child(2)').css('color', 'white');
   }
 
   previousTransactionRowSelected = this;
 
 });
+
+$(document.body).keydown(function(e){
+  if(e.which == 8){ //if the key pressed was "delete"
+    console.log('YO');
+    if(previousTransactionRowSelected.style.borderBottom == 'none, 1px solid rgb(125, 170, 108)'){
+      $(previousTransactionRowSelected).remove();
+    }
+  }
+})
